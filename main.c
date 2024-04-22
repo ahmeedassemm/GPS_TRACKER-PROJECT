df0cc7faac1a9f5907701cd0b87a328f98408c06
KhalidAlansary <khaled.ansary2003@outlook.com>
2024-04-22 17:39:06 +0200

    Moved initializations outside the while loop

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 #include "UART.h"
 
 int main() {
-    while (1) {
     GPIO_initPORTF();
-    // Testing turning on all LEDs
-    led_on(GPIO_ALL_LEDS);
     UART7_INIT();
 
+    while (1) {
+    // Testing turning on all LEDs
+    led_on(GPIO_ALL_LEDS);
     }
 }

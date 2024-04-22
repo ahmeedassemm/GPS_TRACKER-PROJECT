624e2de689c7aca979d788a7db70bc8ca800985b
KhalidAlansary <khaled.ansary2003@outlook.com>
2024-04-22 09:19:07 +0200

    Applied code formatting and fixed some indentations.

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include "GPIO.h"
+#include "UART.h"
 
 int main() {
     while (1) {
     GPIO_initPORTF();
     // Testing turning on all LEDs
     led_on(GPIO_ALL_LEDS);
+    UART7_INIT();
+
     }
 }

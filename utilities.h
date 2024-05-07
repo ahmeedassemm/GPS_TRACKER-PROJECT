34e3536fd6cdb4c001df1c739a8a376e1b61d357
Ahmed Assem <116248609+ahmeedassemm@users.noreply.github.com>
2024-05-07 23:35:11 +0300

    adds utilities header file

---
+++ utilities.h
@@ -0,0 +1,5 @@
+double to_degree(float raw_degree);
+double to_radians(double degrees);
+double distance(double lat1, double lon1, double lat2, double lon2);
+int approximate(double a, float d);
+void UART0SendFloat(float num);

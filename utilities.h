f82df6a61ca93b6f0ce56c7dcacf3037efc53df5
mariamahmedd3 <110908406+mariamahmedd3@users.noreply.github.com>
2024-05-14 23:21:41 +0300

    utilities.h

--- utilities.h
+++ utilities.h
@@ -1,5 +1,10 @@
+#ifndef UTILITIES_H_INCLUDED
+#define UTILITIES_H_INCLUDED
+
 double to_degree(float raw_degree);
 double to_radians(double degrees);
 double distance(double lat1, double lon1, double lat2, double lon2);
 int approximate(double a, float d);
 void UART0SendFloat(float num);
+
+#endif

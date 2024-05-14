95aa129fb0eb108c5a6eea3c09fd316f6dbd185e
mariamahmedd3 <110908406+mariamahmedd3@users.noreply.github.com>
2024-05-14 23:22:36 +0300

    utilities.c

--- utilities.c
+++ utilities.c
@@ -1,24 +1,26 @@
 #include <math.h>
+#include <stdio.h>
 
 #include "tm4c123gh6pm.h"
 
 #define M_PI 3.14159265358979323846
 #define RADIUS 6371000 // radius of earth in meters
 
-double to_degree(float raw_degree) {  //gps output to degrees
+//gps output to degrees
+double to_degree(float raw_degree) {
     int dd = (int)(raw_degree / 100);
     double mm = raw_degree - (dd * 100);
     double degree = dd + (mm / 60);
     return degree;
 }
 
-
-double to_radians(double degrees) {   // degree to rad
+// degree to rad
+double to_radians(double degrees) {
     return degrees * M_PI / 180.0;
 }
 
-
-double distance(double lat1, double lon1, double lat2, double lon2) {  //calc distance between 2 coordinates in M
+//calc distance between 2 coordinates in M
+double distance(double lat1, double lon1, double lat2, double lon2) {
     double dlat = to_radians(lat2 - lat1);
     double dlon = to_radians(lon2 - lon1);
     double a = pow(sin(dlat / 2), 2) + cos(to_radians(lat1)) * cos(to_radians(lat2)) * pow(sin(dlon / 2), 2);
@@ -26,11 +28,9 @@ double distance(double lat1, double lon1, double lat2, double lon2) {  //calc di
     return RADIUS * c;
 }
 
-
 int approximate(double a, float d) {
     return (int)(a / d + 0.5) * d;
 }
-
 
 void UART0SendFloat(float num) {
     int i;
